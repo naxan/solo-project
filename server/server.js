1719ac828d6be1a667e31acdb0cef68545eb1462
akempel <anna.kb.kempel@gmail.com>
2024-09-18 14:01:20 -0400

    Got webpack working! The server and frontend bundle now communicate

--- server/server.js
+++ server/server.js
@@ -9,6 +9,18 @@ app.use(express.json());
 // serve static files
 app.use('/', express.static(path.resolve(__dirname, '../client')));
 
+// global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Unknown middleware error',
+    status: 500,
+    message: { err: 'There was an error' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
 // spin up server
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+module.exports = {
+  entry: './client/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env', '@babel/preset-react'],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'client'),
+    },
+    proxy: [
+      {
+        context: ['/api'],
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        logLevel: 'info',
+      },
+    ],
+    port: 8080,
+  },
+};
