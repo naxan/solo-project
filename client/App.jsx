0f83d7988654dcc67afd388cc55c805040d6393a
akempel <anna.kb.kempel@gmail.com>
2024-09-18 20:50:50 -0400

    Creates new pages components and sets up basic routes

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// want to use useNavigate for components that I want to navigate via the react router
+
+import HomePage from './pages/HomePage';
+import RecipePage from './pages/RecipePage';
+import PlannerPage from './pages/PlannerPage';
 
 import HeaderNav from './components/HeaderNav';
 import RecipeContainer from './containers/RecipeContainer';
@@ -7,13 +12,14 @@ import RecipeContainer from './containers/RecipeContainer';
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <h1>Welcome to the App!</h1>
-        <Link to='/recipes'>Recipes</Link>
-      </div>
+      <HeaderNav />
+      <Link to='/'>Home</Link>
+      <Link to='/recipes'>Recipes</Link>
+      <Link to='/planner'>Planner</Link>
       <Routes>
-        <Route path='/' element={<HeaderNav />}></Route>
-        <Route path='/recipes' element={<RecipeContainer />}></Route>
+        <Route path='/' element={<HomePage />}></Route>
+        <Route path='/recipes' element={<RecipePage />}></Route>
+        <Route path='/planner' element={<PlannerPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/pages/PlannerPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlannerPage = () => {
+  return (
+    <div>
+      <h2>Planner Page</h2>
+    </div>
+  );
+};
+
+export default PlannerPage;
