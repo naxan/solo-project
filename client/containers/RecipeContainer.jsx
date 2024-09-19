385852dcdcea86ef1c20800a99c2c03287b1e590
akempel <anna.kb.kempel@gmail.com>
2024-09-19 17:40:45 -0400

    Adds full search bar functionality

--- client/containers/RecipeContainer.jsx
+++ client/containers/RecipeContainer.jsx
@@ -9,10 +9,28 @@ import RecipeCard from '../components/RecipeCard';
 const RecipeContainer = () => {
   // get recipe card array state
   const recipeList = useSelector((state) => state.recipes.recipeList);
+  const search = useSelector((state) => state.recipes.search);
+
+  // if search is empty, show all recipe cards
+  // if search is not empty, filter recipe list by those whose titles include the search string
+
   const recipeCards = [];
-  for (let i = 0; i < recipeList.length; i++) {
-    const recipe = recipeList[i];
-    recipeCards.push(<RecipeCard key={recipe.id} {...recipe} />);
+
+  if (search.length === 0) {
+    for (let i = 0; i < recipeList.length; i++) {
+      const recipe = recipeList[i];
+      recipeCards.push(<RecipeCard key={recipe.id} {...recipe} />);
+    }
+  } else {
+    for (let i = 0; i < recipeList.length; i++) {
+      const recipe = recipeList[i];
+      const title = recipe.title.toUpperCase();
+      const currentSearch = search.toUpperCase();
+      console.log(title, currentSearch);
+      if (title.includes(currentSearch)) {
+        recipeCards.push(<RecipeCard key={recipe.id} {...recipe} />);
+      }
+    }
   }
 
   return <div id='recipe-container'>{recipeCards}</div>;
